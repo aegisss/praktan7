145cbe0265b531470fa4ba03cfa624d4177fd1c0
aegisss <113986232+aegisss@users.noreply.github.com>
2023-02-09 16:53:43 +0300

    Add files via upload

---
+++ Drive/Drives.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive
+{
+    internal class Drives
+    {
+        public static List<System.IO.DriveInfo> disks = new List<System.IO.DriveInfo>();
+        public static List<string> paths = new List<string>();
+        public static List<string> lastPath = new List<string>();
+        public static int Drivers()
+        {
+            int ct = 0;
+            lastPath.Clear();
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.IsReady == true)
+                {
+                    Console.WriteLine($"    {d.Name}      {d.TotalSize} bytes");
+                }
+                else
+                {
+                    Console.WriteLine($"    {d.Name}");
+                }
+                ct++;
+                disks.Add(d);
+            }
+            return ct;
+        }
+
+        public static void getDrivers(string path)
+        {
+            try{
+                lastPath.Add(path);
+                paths.Clear();
+                string[] dirs = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine($"    {s}");
+                    paths.Add(s);
+                }
+                if (files.Length != 0)
+                {
+                    foreach (string s in files)
+                    {
+                        Console.WriteLine($"    {s}");
+                        paths.Add(s);
+                    }
+                }
+                
+
+            }
+            catch (IOException)
+            {
+
+            }
+            
+        }
+
+
+    }
+}
---
+++ Drive/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Drive
+{
+    public class Program
+    {
+
+        static void Main(string[] argrs)
+        {
+
+            int a = Drives.Drivers();
+            while (true)
+            {
+                int disk = Arrow.Move(a);
+
+
+                if (disk == -1)
+                {
+                    Console.Clear();
+
+                    if (Drives.lastPath.Count() == 1)
+                    {
+                        a = Drives.Drivers();
+                        Drives.paths.Clear();
+
+                        Arrow.pose = 0;
+                    }
+                    else if (Drives.lastPath.Count() > 1)
+                    {
+                        int len = Drives.lastPath.Count() - 1;
+
+                        string last = Drives.lastPath[len - 1];
+
+                        Drives.lastPath.RemoveAt(len);
+                        Drives.lastPath.RemoveAt(len - 1);
+                        Drives.getDrivers(last);
+                        
+
+
+                        a = Drives.paths.Count();
+
+                        Arrow.pose = 0;
+
+                    }
+                }
+
+                else if (Drives.paths.Count() == 0)
+                {
+                    Console.Clear();
+                    Drives.getDrivers(Drives.disks[0].Name);
+                    a = Drives.paths.Count();
+                }
+
+                else
+                {
+                    Console.Clear();
+                    Drives.getDrivers(Drives.paths[disk]);
+                    a = Drives.paths.Count();
+                    Arrow.pose = 0;
+
+                }
+                
+                
+
+
+
+            }
+        }
+    }
+}
